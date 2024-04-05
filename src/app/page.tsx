d949cc0c6e1d8434c2c6ecd5ac285b694f55ce0f
NightravenJames <jamesnight1994@gmail.com>
2024-04-05 22:36:45 +0300

    Hello world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,5 @@
 'use client'
-import { useEffect } from "react";
-
-
-async function upload(){
-  try {
-    const result = await uploadData({
-      key: filename,
-      data: file,
-      options: {
-        accessLevel: 'guest', // defaults to `guest` but can be 'private' | 'protected' | 'guest'
-        onProgress // Optional progress callback.
-      }
-    }).result;
-    console.log('Succeeded: ', result);
-  } catch (error) {
-    console.log('Error : ', error);
-  }
-}
 export default function Home() {
-  useEffect(()=> {
-    console.log("Woking")
-  },[])
-  return (
-    <button>
-      Click me
-    </button>
-  )
-}
-function uploadData(arg0: {
-  key: any; data: any; options: {
-    accessLevel: string; // defaults to `guest` but can be 'private' | 'protected' | 'guest'
-    onProgress: any; // Optional progress callback.
-  };
-}) {
-  throw new Error("Function not implemented.");
+  return "Hello world"
 }
 
