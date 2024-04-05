4a1e660c6418e1fe0227f4cec9059afde1411ed0
NightravenJames <jamesnight1994@gmail.com>
2024-04-05 17:31:47 +0300

    storage added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "docassistanta2f5c23d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s33444f1d1": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
