f81597c7455515bede4439eb8686d5ffba534696
NightravenJames <jamesnight1994@gmail.com>
2024-04-06 00:27:41 +0300

    reset to hello world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
 'use client'
 
-import { Authenticator } from "@aws-amplify/ui-react"
-import { Amplify } from "aws-amplify"
-import config from '@/amplifyconfiguration.json';
-
-Amplify.configure(config)
 export default function Home() {
-  return (
-    <Authenticator>
-      {({user}) => (
-        <main>
-          Hello {user?.username}
-        </main>
-      )}
-    </Authenticator>
-  )
+  return "Hello World"
 }
 
